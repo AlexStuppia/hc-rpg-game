4fe7e0859cb803f0b9577edcb58a67a595ae6b0b
Hollow126 <tizigess@gmail.com>
2023-12-09 18:22:14 +0100

    imput utente con comeback completo, ragionare se passare solo gli id o se ha senso utilizzare gli oggetti direttamente

--- src/main/java/org/ht/rpg/game/utils/CombatUtils.java
+++ src/main/java/org/ht/rpg/game/utils/CombatUtils.java
@@ -32,50 +32,74 @@ public class CombatUtils {
 
     public List<Choiche> makeAllPlayerChooseAction(Party parties) {
         tastiera = new Scanner(System.in);
-        List<Choiche> choichesOfAllPlayer = null;
-        for (int i = 0; i < parties.getAllyList().size() ; i++) {
-
-
-        }
-        for (Fighter member : parties.getAllyList()) {
-
-            System.out.println("che cosa vuoi fare " + member.getId() + " ?");
-            System.out.println("1 attacck enemy ");
+        List<Choiche> choichesOfAllAlly = new ArrayList<>();
+        int i = 0;
+        while (i < parties.getAllyList().size()) {
+            Ally member = parties.getAllyList().get(i);
+            System.out.println("what you want to do " + member.getName() + " ?");
+            System.out.println("1 attack enemy ");
             System.out.println("2 use a magic ");
             System.out.println("3 use a item  ");
             System.out.println("4 run away");
-            System.out.println("0 go back to the pg precedente ");
-
-            Integer scelta = Integer.parseInt(tastiera.nextLine());
-            switch (scelta) {
+            if (i != 0) {
+                System.out.println("0 go back to the previous pg");
+            }
+            int userInput = Integer.parseInt(tastiera.nextLine());
+            switch (userInput) {
                 case 1: {
                     Attack chosenAttack = selectAttack(member);
-                    List<Integer> listOfTarget = selectTargets(parties, chosenAttack);
-                    Choiche singlechoiche = new Choiche(member.getId(), chosenAttack.getId(), listOfTarget, chosenAttack.getPriority(), chosenAttack.getPriorityLast());
-                    choichesOfAllPlayer.add(singlechoiche);
+                    if (chosenAttack.getId() == 0) {
+                        System.out.println("return to selection action");
+                    } else {
+                        List<Integer> listOfTarget = selectTargets(parties, chosenAttack);
+                        if (listOfTarget.contains(0)) {
+                            System.out.println("return to selection actions");
+                        } else {
+                            Choiche singlechoiche = new Choiche(member.getId(), chosenAttack.getId(), listOfTarget,
+                                    chosenAttack.getPriority(), chosenAttack.getPriorityLast());
+                            choichesOfAllAlly.add(singlechoiche);
+                            i++;
+                        }
+                    }
+                    break;
                 }
-                // idem per il resto
+                case 0: {
+                    if (i == 0) {
+                        System.out.println("wrong input");
+                    } else {
+                        i--;
+                        choichesOfAllAlly.remove(i);
+                    }
+                    break;
+                }
             }
         }
-        return choichesOfAllPlayer;
+        return choichesOfAllAlly;
     }
+
 
     private Attack selectAttack(Fighter member) {
         // if member is an ally, scelta nostra
-        // if is an enemy,
+        // if is an enemy fare altro metodo con IA
         Attack chosenAttack = null;
+        Attack discardAttack = new Attack(0, "discard", "utility", 10, 10, true, true, true, true, 1, true, true, true);
         System.out.println("Select an attack:");
-        Integer inputPlayer = Integer.parseInt(tastiera.nextLine());
+        System.out.println("Select 0 to go back to action selection");
         int contatore = 1;
         for (Attack attack : member.getAttacks()) {
             System.out.println(contatore + ": " + attack.getName() + attack.getEffect());
             contatore++;
         }
-        inputPlayer = Integer.parseInt(tastiera.nextLine());
+        Integer inputPlayer = Integer.parseInt(tastiera.nextLine());
         if (inputPlayer > 0 && inputPlayer <= member.getAttacks().size()) {
             chosenAttack = member.getAttacks().get(inputPlayer - 1);
+        } else if (inputPlayer == 0) {
+            System.out.println("go back to action selection");
+            return discardAttack;
         } else {
-            System.out.println("wrong input retry");
+            System.out.println("wrong input");
+            System.out.println("go back to action selection");
+            return discardAttack;
         }
         return chosenAttack;
     }
@@ -83,24 +107,30 @@ public class CombatUtils {
     private List<Integer> selectTargets(Party parties, Attack chosenAttack) {
         List<Integer> targets = new ArrayList<>();
         System.out.println("Select a target:");
-        if (chosenAttack.getHittingAllPlayer() == true) {
+        if (chosenAttack.getHittingAllPlayer()) {
             System.out.println("this attack will target everyone on the field");
             System.out.println("1 if you are sure, 0 if you want go back");
+            Integer userInput = Integer.parseInt(tastiera.nextLine());
+            if (userInput == 1) {
+                for (Ally ally : parties.getAllyList()) {
+                    targets.add(ally.getId());
+                }
+                for (Enemy enemy : parties.getEnemyList()) {
+                    targets.add(enemy.getId());
+                }
+            } else {
+                System.out.println("go back to selection");
+                targets.add(0);
+            }
+        } else if (chosenAttack.getHittingAllAlly()) {
             for (Ally ally : parties.getAllyList()) {
                 targets.add(ally.getId());
             }
+        } else if (chosenAttack.getHittingAllEnemy()) {
             for (Enemy enemy : parties.getEnemyList()) {
                 targets.add(enemy.getId());
             }
-        } else if (chosenAttack.getHittingAllAlly() == true) {
-            for (Ally ally : parties.getAllyList()) {
-                targets.add(ally.getId());
-            }
-        } else if (chosenAttack.getHittingAllEnemy() == true) {
-            for (Enemy enemy : parties.getEnemyList()) {
-                targets.add(enemy.getId());
-            }
-        } else if (chosenAttack.getCanAttackMultiTarget() == true) {
+        } else if (chosenAttack.getCanAttackMultiTarget()) {
             for (int i = 0; i < chosenAttack.getNumberOfTarget(); i++) {
                 System.out.println("select " + chosenAttack.getNumberOfTarget() + " target");
                 int contatore = 1;
@@ -114,6 +144,8 @@ public class CombatUtils {
                     targets.add(tempEnemyList.get(inputPlayer - 1).getId());
                 } else {
                     System.out.println("wrong input retry");
+                    targets.add(0);
+                    break;
                 }
             }
         } else if (chosenAttack.getCanAttackMultiTarget() == false && chosenAttack.getCanTargetAlly() == false) {
@@ -128,11 +160,11 @@ public class CombatUtils {
                 targets.add(tempEnemyList.get(inputPlayer - 1).getId());
             } else {
                 System.out.println("wrong input retry");
+                targets.add(0);
             }
         }
         return targets;
     }
-
 
 
     public Map<Integer, Integer> calculateVelocity(Party parties, List<Choiche> fighterActions) {
@@ -177,9 +209,22 @@ public class CombatUtils {
         }
         return orderedMap;
     }
-    Integer inputPlayer = Integer.parseInt(tastiera.nextLine());
 
     public void makeActionsDO(Map<Integer, Integer> velocityOrder, List<Choiche> choichesOfAll) {
+//        velocityOrder.entrySet().forEach(entry -> {
+//            int temporaryid = entry.getKey();
+//            choichesOfAll.get().getIdSender()
+//        });
+
+
+        // forse conviene che choiche invece di avere degli id e basta, riceva in pasto
+        // proprio gli interi oggetti
+
+        for (Choiche choice : choichesOfAll) {
+            System.out.println("il pg con id " + choice.getIdSender() + " con la mossa " + choice.getIdAction() + " attachera " +
+                    "" + choice.getListOfTargets());
+        }
+        System.out.println("");
         System.out.println("porcodiooooo");
     }
 }
